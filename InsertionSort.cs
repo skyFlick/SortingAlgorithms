9046a4ff361e23ecbe62909ad6164ff1a944954e
skyFlick <51884204+skyFlick@users.noreply.github.com>
2019-06-16 09:32:27 -0700

    Add files via upload

---
+++ InsertionSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = establishArray(100);
+
+            Console.WriteLine("\n\nUnsorted array: \n");
+            printArray(array);
+
+            sortArray_InsertionSort(array);
+
+            Console.WriteLine("\n\nSorted array: \n");
+            printArray(array);
+        }
+
+        private static void sortArray_InsertionSort(int[] array)
+        {
+            for(int i = 1; i < array.Length; i++)
+            {
+                int x = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > x)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = x;
+            }
+        }
+
+        private static void printArray(int[] array)
+        {
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + ", ");
+            }
+        }
+
+        private static int[] establishArray(int n)
+        {
+            int[] array = new int[n];
+            Random r = new Random();
+
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = r.Next(1,1000);
+            }
+
+            return array;
+        }
+    }
+}
